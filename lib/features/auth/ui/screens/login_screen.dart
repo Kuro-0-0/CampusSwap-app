036993fc26f7991d02a9d728876694d8b1596b7f
mauroz9 <mauro.serranoo26@gmail.com>
2026-02-19 13:33:45 +0100

    añadida la navegación a home si se realiza la autenticación

--- lib/features/auth/ui/screens/login_screen.dart
+++ lib/features/auth/ui/screens/login_screen.dart
@@ -2,6 +2,7 @@ import 'package:campusswap_app/core/theme/app_colors.dart';
 import 'package:campusswap_app/features/auth/bloc/auth_bloc.dart';
 import 'package:campusswap_app/features/auth/ui/widgets/login_form.dart';
 import 'package:campusswap_app/features/auth/ui/widgets/login_header.dart';
+import 'package:campusswap_app/main_layout.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -9,9 +10,9 @@ class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   void _navigateToHome(BuildContext context) {
-    // Navigator.of(context).pushReplacement(
-    //   MaterialPageRoute(builder: (context) => const MainLayout()),
-    // );
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (context) => const MainLayout()),
+    );
   }
 
   @override
