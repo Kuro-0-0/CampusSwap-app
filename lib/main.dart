faa7589ca41a8f1deeb8b4cdab8997002e0b1e57
Mauro Serrano <mauro.serranoo26@gmail.com>
2026-02-24 17:23:01 +0100

    feat: inyectar el blocprovider de auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:campusswap_app/features/auth/bloc/auth_bloc.dart';
 import 'package:campusswap_app/features/auth/ui/screens/login_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(BlocProvider(
+      create: (context) => AuthBloc(),
+      child: const MainApp(),
+    ),);
 }
 
 class MainApp extends StatelessWidget {
